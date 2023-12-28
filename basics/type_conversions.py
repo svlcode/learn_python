fdc514a0392111fcd23827a88c6fe39608700c4f
Vio <vio.slaniceanu@gmail.com>
2023-12-28 12:39:00 +0200

    add type_conversions

---
+++ basics/type_conversions.py
@@ -0,0 +1,14 @@
+x = 0
+print(bool(x))
+
+
+x = input("x: ")
+
+# if the input is '0' the conversion will return True
+conversion = bool(x)
+
+print(conversion)
+
+
+# print(int(x))
+# print(float(x))
