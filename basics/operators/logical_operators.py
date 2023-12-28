5a976d5d154d2aa9dedeb10d913553d36815c9ce
Vio <vio.slaniceanu@gmail.com>
2023-12-28 12:40:58 +0200

    add logical operators

---
+++ basics/operators/logical_operators.py
@@ -0,0 +1,18 @@
+name = "Vio"
+
+# Falsy values:
+# ""
+# 0
+# None
+# []
+
+if not name:
+    print("Name is empty")
+else:
+    print("Name is not empty")
+
+age = 22
+
+# chaining operators
+if 18 <= age < 65:
+    print("you are eligible")
